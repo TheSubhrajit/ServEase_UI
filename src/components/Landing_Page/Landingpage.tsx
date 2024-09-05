ac8d03e0a264c1e50da3e33cb6a0fa7befe02a09
Subir-Droid <subirdeyabhi@gmail.com>
2024-09-06 00:06:52 +0530

    add event listeners

--- src/components/Landing_Page/Landingpage.tsx
+++ src/components/Landing_Page/Landingpage.tsx
@@ -2,21 +2,26 @@ import { Tooltip } from '@mui/material'
 import './Landingpage.css'
 
 export const Landingpage: any = () =>{
+
+    const handleSelectionClick = (e:any) =>{
+        console.log(e)
+        
+    }
     
     return (
         <section className="landing-container">
             <Tooltip title="Cook" arrow>
-            <div className='selectors'>
+            <div className='selectors' onClick={e =>  handleSelectionClick('cook')}>
             <img src='../cooking.png'></img>
             </div>
             </Tooltip>
             <Tooltip title="Maid" arrow>
-            <div className='selectors'>
+            <div className='selectors' onClick={e =>  handleSelectionClick('maid')}>
                 <img src='../cleaner.png'></img>
             </div>
             </Tooltip>
             <Tooltip title="Nanny" arrow>
-            <div className='selectors'>
+            <div className='selectors' onClick={e =>  handleSelectionClick('nanny')}>
             <img src='../babysitter.png'></img>
             </div>
             </Tooltip>
