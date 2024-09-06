489c4d8872519e000ae3b67e45831ef8fd8717ca
Subir-Droid <subirdeyabhi@gmail.com>
2024-09-06 15:51:09 +0530

    end of 2 screens ( very raw )

--- src/components/DetailsView/DetailsView.tsx
+++ src/components/DetailsView/DetailsView.tsx
@@ -1,16 +1,68 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import './DetailsView.css';
+import { useEffect, useState } from 'react';
+import { ServiceProvidersDetails } from '../ServiceProvidersDetails/ServiceProvidersDetails';
+import { Help_Search } from '../Help-Search/Help_Search';
+
 interface ChildComponentProps {
     sendDataToParent: (data: string) => void; // Adjust the type if needed
   }
 
 export const DetailsView:React.FC<ChildComponentProps> = ({ sendDataToParent }) =>{
 
+    const [ServiceProvidersData , setServiceProvidersData] = useState([]);
+
+    const [open, setOpen] = useState(false);
+
     const handleBackClick = () =>{
             sendDataToParent('')
     }
+
+    const handleSearchClick = () => {
+        console.log("handleSearch")
+        setOpen(true);
+      };
+    
+      const handleClose = (value: string) => {
+        setOpen(false);
+      };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://freetestapi.com/api/v1/users');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setServiceProvidersData(data);
+                console.log("ServiceProvidersData =>",ServiceProvidersData)
+            } catch (error) {
+                console.error('There was a problem with the fetch operation:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return(   
         <>
-        <div>
-            <button onClick={handleBackClick}> Back </button>
+        <div className="details-view">
+            <header className='headers'>
+            <Button onClick={handleBackClick} variant="outlined">Back</Button>
+            <Button variant="outlined" onClick={handleSearchClick}>Search</Button>
+            </header>
+            
+            <div className='providers-view'> 
+            {ServiceProvidersData.map(serviceproviders =>{
+                 return <div className='views'>
+                    <ServiceProvidersDetails key={serviceproviders['id']} props={serviceproviders} ></ServiceProvidersDetails>
+                 </div>
+            })}
+            </div>
+            
+           <Help_Search open={open} selectedValue={''} onClose={handleClose}></Help_Search>
+            
         </div>
         </>
     )
