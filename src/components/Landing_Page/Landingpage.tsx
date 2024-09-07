e41414a5520e3740491de774dcfeee5b90276a15
Subir-Droid <subirdeyabhi@gmail.com>
2024-09-08 00:20:55 +0530

    add changes

--- src/components/Landing_Page/Landingpage.tsx
+++ src/components/Landing_Page/Landingpage.tsx
@@ -1,5 +1,10 @@
 import { Tooltip } from '@mui/material'
 import './Landingpage.css'
+
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+  } from 'react-places-autocomplete';
 import { useState } from 'react';
 
 interface ChildComponentProps {
@@ -7,10 +12,24 @@ interface ChildComponentProps {
   }
 
 export const Landingpage: React.FC<ChildComponentProps> = ({ sendDataToParent }) =>{
+
+    const [state , setstate ] = useState({address: ''});
+
+
     const handleClick = (e:any) =>{
         sendDataToParent(e)
     }
 
+    const handleSelect = address => {
+        geocodeByAddress(address)
+          .then(results => getLatLng(results[0]))
+          .then(latLng => console.log('Success', latLng))
+          .catch(error => console.error('Error', error));
+      };
+
+      const handleChange = address => {
+        setstate({ address });
+      };
    
     
     
@@ -31,6 +50,45 @@ export const Landingpage: React.FC<ChildComponentProps> = ({ sendDataToParent })
             <img src='../babysitter.png'></img>
             </div>
             </Tooltip>
+
+            <PlacesAutocomplete
+        value={state.address}
+        onChange={handleChange}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
             
         </section>
     )
