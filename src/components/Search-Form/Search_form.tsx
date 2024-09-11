bf7975451c23cef03b2b4a4d92a70fb787739c38
My Name <thesubhrajitd2001@gmail.com>
2024-09-11 19:54:23 +0530

    search-form

--- src/components/Search-Form/Search_form.tsx
+++ src/components/Search-Form/Search_form.tsx
@@ -32,66 +32,9 @@ export const Search_form = (props: SimpleDialogProps) => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="all">
             <div className="flex-container1">
-              {/* <div className="gender">
-                <div className="men">
-                  <Form.Check
-                    {...register("Men Selected")}
-                    id="custom-switch"
-                    label="Men"
-                  />
-                  <div className="icon-div">
-                    <Button variant="dark">
-                      <img src="man-user-circle-icon.png"></img>
-                    </Button>
-                  </div>
-                </div>
-                <div className="women">
-                  <Form.Check
-                    {...register("Women Selected")}
-                    id="custom-switch"
-                    label="Women"
-                  />
-                  <div className="icon-div">
-                    <Button variant="dark">
-                      <img src="woman-user-color-icon.png"></img>
-                    </Button>
-                  </div>
-                </div>
-              </div>s
-              <div className="sage">
-                <div className="slider-1">
-                  <Box sx={{ width: 300 }}>
-                    <Slider
-                      defaultValue={20}
-                      {...register("Selected Men Age:")}
-                      getAriaValueText={valuetext}
-                      valueLabelDisplay="auto"
-                      step={1}
-                      marks
-                      min={18}
-                      max={60}
-                    />
-                  </Box>
-                </div>
-                <div className="slider-2">
-                  <Box sx={{ width: 300 }}>
-                    <Slider
-                      defaultValue={20}
-                      {...register("Selected Women Age:")}
-                      getAriaValueText={valuetext}
-                      valueLabelDisplay="auto"
-                      step={1}
-                      marks
-                      min={18}
-                      max={60}
-                    />
-                  </Box>
-                </div>
-              </div> */}
-
               <div className="item-viewer">
                 <div className="checkbox-style">
-                <Form.Check
+                  <Form.Check
                     {...register("Men Selected")}
                     id="custom-switch"
                     label="Men"
@@ -99,15 +42,12 @@ export const Search_form = (props: SimpleDialogProps) => {
                 </div>
 
                 <div className="checkbox-style">
-                <Button variant="dark" className="icon-div">
-                      <img src="man-user-circle-icon.png"></img>
-                    </Button>
+                  <Button variant="dark" className="icon-div">
+                    <img src="man-user-circle-icon.png"></img>
+                  </Button>
                 </div>
-              
-
-
                 <div className="checkbox-style">
-                <Box sx={{ width: 300 }}>
+                  <Box sx={{ width: 300 }}>
                     <Slider
                       defaultValue={20}
                       {...register("Selected Men Age:")}
@@ -120,74 +60,106 @@ export const Search_form = (props: SimpleDialogProps) => {
                     />
                   </Box>
                 </div>
-                    
-
               </div>
             </div>
             <div className="flex-container1">
-              {/* <div className="time">
-                <div className="day">
-                  <Form.Check
-                    {...register("checkbox")}
-                    id="custom-switch"
-                    label="Day"
-                  />
-                  <div className="icon-div">
-                    <Button variant="dark">
-                      <img src="day.png"></img>
-                    </Button>
-                  </div>
-                </div>
-                <div className="night">
-                  <Form.Check
-                    {...register("checkbox")}
-                    id="custom-switch"
-                    label="Night"
-                  />
-                  <div className="icon-div">
-                    <Button variant="dark">
-                      <img src="night.png"></img>
-                    </Button>
-                  </div>
-                </div>
+            <div className="item-viewer">
+            <div className="checkbox-style">
+                <Form.Check
+                  {...register("Women Selected")}
+                  id="custom-switch"
+                  label="Women"
+                />
               </div>
-              <div className="times">
-                <div className="time1">
-                  <Box sx={{ width: 300 }}>
-                    <Slider
-                      defaultValue={6}
-                      color="secondary"
-                      {...register("Morning Timing")}
-                      getAriaValueText={valuetext}
-                      aria-label="Time Between"
-                      valueLabelDisplay="auto"
-                      step={1}
-                      marks
-                      min={6}
-                      max={12}
-                    />
-                  </Box>
-                </div>
-                <div className="time1">
-                  <Box sx={{ width: 300 }}>
-                    <Slider
-                      defaultValue={4}
-                      color="secondary"
-                      {...register("Night Timing")}
-                      getAriaValueText={valuetext}
-                      valueLabelDisplay="auto"
-                      step={1}
-                      marks
-                      min={4}
-                      max={10}
-                    />
-                  </Box>
-                </div>
-              </div> */}
+              <div className="checkbox-style">
+                <Button variant="dark" className="icon-div">
+                  <img src="woman-user-color-icon.png"></img>
+                </Button>
+              </div>
+
+              <div className="checkbox-style">
+               <Box sx={{ width: 300 }}>
+                  <Slider
+                    defaultValue={20}
+                    {...register("Selected Men Age:")}
+                    getAriaValueText={valuetext}
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    min={18}
+                    max={60}
+                  />
+                </Box>
+              </div>
+            </div>
+            </div>
+            <div className="flex-container1">
+            <div className="item-viewer">
+            <div className="checkbox-style">
+                <Form.Check
+                  {...register("checkbox")}
+                  id="custom-switch"
+                  label="Day"
+                />
+              </div>
+              <div className="checkbox-style">
+                <Button variant="dark" className="icon-div">
+                  <img src="day.png"></img>
+                </Button>
+              </div>
+              <div className="checkbox-style">
+                <Box sx={{ width: 300 }}>
+                  <Slider
+                    defaultValue={6}
+                    color="secondary"
+                    {...register("Morning Timing")}
+                    getAriaValueText={valuetext}
+                    aria-label="Time Between"
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    min={6}
+                    max={12}
+                  />
+                </Box>
+              </div>
+            </div>
+            </div>
+            <div className="flex-container1">
+            <div className="item-viewer">
+            <div className="checkbox-style">
+                <Form.Check
+                  {...register("checkbox")}
+                  id="custom-switch"
+                  label="Night"
+                />
+              </div>
+
+              <div className="checkbox-style">
+                <Button variant="dark" className="icon-div">
+                  <img src="night.png"></img>
+                </Button>
+              </div>
+              <div className="checkbox-style">
+                <Box sx={{ width: 300 }}>
+                  <Slider
+                    defaultValue={4}
+                    color="secondary"
+                    {...register("Night Timing")}
+                    getAriaValueText={valuetext}
+                    valueLabelDisplay="auto"
+                    step={1}
+                    marks
+                    min={4}
+                    max={10}
+                  />
+                </Box>
+              </div>
+            </div>
             </div>
             <div className="footer">
               <div className="Sbtn1">
-                <Button type="submit" id="button1"  variant="outline-dark">
+                <Button type="submit" id="button1" variant="outline-dark">
                   Submit
                 </Button>
               </div>
