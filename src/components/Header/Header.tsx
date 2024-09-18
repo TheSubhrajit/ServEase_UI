4bbf8a8a0b62c495dccbe9434476379c6873f108
My Name <thesubhrajitd2001@gmail.com>
2024-09-18 21:36:04 +0530

    Header part

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,38 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+
 
 export const Header = () => {
+  
   return (
     <>
-      <Navbar bg="white" data-bs-theme="white">
-        <Container>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container className="header">
           <Navbar.Brand href="#home" className="h1">
            <h1>ServEase</h1>
            <h4>innovative</h4>
           </Navbar.Brand>
-          {/* <Nav className="me-auto">
-            <Nav.Link href="#home">Cooks</Nav.Link>
-            <Nav.Link href="#features">Maids</Nav.Link>
-            <Nav.Link href="#pricing">Nanny</Nav.Link>
-          </Nav> */}
+          </Container>
+          
+          <DropdownButton id="dropdown-basic-button"
+           title="Location" className="header">
+          <Dropdown.Item  href="#/action-1"></Dropdown.Item>
+           </DropdownButton>
 
-
-        </Container>
-      </Navbar>
+          <DropdownButton id="dropdown-basic-button"
+           title="My Account" className="header">
+      <Dropdown.Item  href="#/action-1">Login / Register</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Privacy Policy</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Notification</Dropdown.Item>
+      <Dropdown.Item href="#/action-4">Sign Out</Dropdown.Item>
+    </DropdownButton>
+    
+    </Navbar>
+      
     </>
   );
 };
