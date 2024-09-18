d59351796131179a7ca2528b3a242047f91068b1
My Name <thesubhrajitd2001@gmail.com>
2024-09-18 15:13:51 +0530

    added animation in landing page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,19 @@ import Navbar from "react-bootstrap/Navbar";
 export const Header = () => {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="white" data-bs-theme="white">
         <Container>
-          <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+          <Navbar.Brand href="#home" className="h1">
+           <h1>ServEase</h1>
+           <h4>innovative</h4>
+          </Navbar.Brand>
           {/* <Nav className="me-auto">
             <Nav.Link href="#home">Cooks</Nav.Link>
             <Nav.Link href="#features">Maids</Nav.Link>
             <Nav.Link href="#pricing">Nanny</Nav.Link>
           </Nav> */}
+
+
         </Container>
       </Navbar>
     </>
