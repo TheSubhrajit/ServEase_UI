4973ad14a0457a86563339f9224fb81040f45806
My Name <thesubhrajitd2001@gmail.com>
2024-09-21 16:41:45 +0530

    New Search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,45 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+// import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
+import "./Header.css";
 
 
 export const Header = () => {
   
   return (
     <>
-    {/*  */}
-      <Navbar bg="dark" data-bs-theme="dark" my-0>
-        <Container className="header">
-          <Navbar.Brand href="#home" className="text">
-           <h1>ServEase</h1>
-           <h3>innovative</h3>
+    {/*bg="dark" data-bs-theme="dark" my-0 w-5  className="text-white py-0 px-0 space-x-0"  */}
+      <Navbar className="header">
+        <Container className="head">
+          <Navbar.Brand href="#home">
+           <h2>Home-ServEase</h2>
+           {/* <h6>Care</h6> */}
           </Navbar.Brand>
           </Container>
-
-          <DropdownButton id="dropdown-basic-button"
-           title="Location" className="header">
-          <Dropdown.Item  href="#/action-1"></Dropdown.Item>
+        <div className="head2">
+          <div className="head3">
+          <DropdownButton id="dropdown-button-dark"
+          title="Location"
+          variant="dark"  >
+          <Dropdown.Item> Choose Your Location</Dropdown.Item>
            </DropdownButton>
-
-          <DropdownButton id="dropdown-basic-button"
-           title="My Account" className="header">
+           </div>
+        <div className="head4">
+          <DropdownButton id="dropdown-button-dark"
+          title="My Account"
+          variant="dark">
       <Dropdown.Item  href="#/action-1">Login / Register</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Privacy Policy</Dropdown.Item>
       <Dropdown.Item href="#/action-3">Notification</Dropdown.Item>
       <Dropdown.Item href="#/action-4">Sign Out</Dropdown.Item>
-    </DropdownButton>
-    
+      </DropdownButton>
+      </div>
+    </div>
     </Navbar>
       
     </>
--- src/components/Search-Form/Search_form.tsx
+++ src/components/Search-Form/Search_form.tsx
@@ -1,180 +1,77 @@
-import * as React from "react";
-import "./Search_form.css";
-import Form from "react-bootstrap/Form";
-import Box from "@mui/material/Box";
-import Slider from "@mui/material/Slider";
-import Button from "react-bootstrap/Button";
-import { useForm } from "react-hook-form";
-import Dialog from "@mui/material/Dialog";
-
-function valuetext(value: Number) {
-  return `${value}°C`;
-}
+import { Dialog } from '@mui/material';
+import React, { useState } from 'react';
 
 export interface SimpleDialogProps {
   open: boolean;
   selectedValue: string;
   onClose: (value: string) => void;
 }
-export const Search_form = (props: SimpleDialogProps) => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (d: any) => alert(JSON.stringify(d));
 
-  const { onClose, selectedValue, open } = props;
+const Search_form = (props: SimpleDialogProps) => {
+    const [age, setAge] = useState<number>(14);
+    const [availability, setAvailability] = useState<string>('8.00 AM');
+    const [areaRange, setAreaRange] = useState<string>('');
+    
+      const { onClose, selectedValue, open } = props;
+    
+      const handleClose = () => {
+        onClose(selectedValue);
+      };
 
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
-
-  return (
-    <>
+    return (
       <Dialog onClose={handleClose} open={open} className="dialog-class">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="all">
-            <div className="flex-container1">
-              <div className="item-viewer">
-                <div className="checkbox-style">
-                  <Form.Check
-                    {...register("Men Selected")}
-                    id="custom-switch"
-                    label="Men"
-                  />
+        <div>
+            <button>Filter ⌛</button>
+            <form>
+                <div>
+                    <label>Gender: </label>
+                    <input type="radio" name="gender" value="Male" /> Male
+                    <input type="radio" name="gender" value="Female" /> Female
                 </div>
-
-                <div className="checkbox-style">
-                  <Button variant="dark" className="icon-div">
-                    <img src="man-user-circle-icon.png"></img>
-                  </Button>
+                <div>
+                    <label>Age: </label>
+                    <input type="range" min="14" max="40" value={age} onChange={e => setAge(Number(e.target.value))} />
+                    {age} years
                 </div>
-                <div className="checkbox-style">
-                  <Box sx={{ width: 300 }}>
-                    <Slider
-                      defaultValue={20}
-                      {...register("Selected Men Age:")}
-                      getAriaValueText={valuetext}
-                      valueLabelDisplay="auto"
-                      step={1}
-                      marks
-                      min={18}
-                      max={60}
-                    />
-                  </Box>
+                <div>
+                    <label>Language: </label>
+                    <input type="checkbox" name="language" value="Bengali" /> Bengali
+                    <input type="checkbox" name="language" value="Hindi" /> Hindi
+                    <input type="checkbox" name="language" value="Kannada" /> Kannada
                 </div>
-              </div>
-            </div>
-            <div className="flex-container1">
-            <div className="item-viewer">
-            <div className="checkbox-style">
-                <Form.Check
-                  {...register("Women Selected")}
-                  id="custom-switch"
-                  label="Women"
-                />
-              </div>
-              <div className="checkbox-style">
-                <Button variant="dark" className="icon-div">
-                  <img src="woman-user-color-icon.png"></img>
-                </Button>
-              </div>
-
-              <div className="checkbox-style">
-               <Box sx={{ width: 300 }}>
-                  <Slider
-                    defaultValue={20}
-                    {...register("Selected Men Age:")}
-                    getAriaValueText={valuetext}
-                    valueLabelDisplay="auto"
-                    step={1}
-                    marks
-                    min={18}
-                    max={60}
-                  />
-                </Box>
-              </div>
-            </div>
-            </div>
-            <div className="flex-container1">
-            <div className="item-viewer">
-            <div className="checkbox-style">
-                <Form.Check
-                  {...register("checkbox")}
-                  id="custom-switch"
-                  label="Day"
-                />
-              </div>
-              <div className="checkbox-style">
-                <Button variant="dark" className="icon-div">
-                  <img src="day.png"></img>
-                </Button>
-              </div>
-              <div className="checkbox-style">
-                <Box sx={{ width: 300 }}>
-                  <Slider
-                    defaultValue={6}
-                    color="secondary"
-                    {...register("Morning Timing")}
-                    getAriaValueText={valuetext}
-                    aria-label="Time Between"
-                    valueLabelDisplay="auto"
-                    step={1}
-                    marks
-                    min={6}
-                    max={12}
-                  />
-                </Box>
-              </div>
-            </div>
-            </div>
-            <div className="flex-container1">
-            <div className="item-viewer">
-            <div className="checkbox-style">
-                <Form.Check
-                  {...register("checkbox")}
-                  id="custom-switch"
-                  label="Night"
-                />
-              </div>
-
-              <div className="checkbox-style">
-                <Button variant="dark" className="icon-div">
-                  <img src="night.png"></img>
-                </Button>
-              </div>
-              <div className="checkbox-style">
-                <Box sx={{ width: 300 }}>
-                  <Slider
-                    defaultValue={4}
-                    color="secondary"
-                    {...register("Night Timing")}
-                    getAriaValueText={valuetext}
-                    valueLabelDisplay="auto"
-                    step={1}
-                    marks
-                    min={4}
-                    max={10}
-                  />
-                </Box>
-              </div>
-            </div>
-            </div>
-            <div className="footer">
-              <div className="Sbtn1">
-                <Button type="submit" id="button1" variant="outline-dark">
-                  Submit
-                </Button>
-              </div>
-              <div className="Sbtn2 ">
-                <Button type="reset" id="button2" variant="outline-dark">
-                  {" "}
-                  Reset{" "}
-                </Button>
-              </div>
-            </div>
-          </div>
-        </form>
-      </Dialog>
-    </>
-  );
+                <div>
+                    <label>Shift Time: </label>
+                    <input type="radio" name="shift" value="Morning" /> Morning
+                    <input type="radio" name="shift" value="Evening" /> Evening
+                </div>
+                <div>
+                    <label>Availability: </label>
+                    <select value={availability} onChange={e => setAvailability(e.target.value)}>
+                        <option value="8.00 AM">8.00 AM</option>
+                        {/* Add more options as needed */}
+                    </select>
+                </div>
+                <div>
+                    <label>Category: </label>
+                    <input type="checkbox" name="category" value="Economy Service" /> Economy Service
+                    <input type="checkbox" name="category" value="Premium Service" /> Premium Service
+                </div>
+                <div>
+                    <label>Customer Ratings: </label>
+                    <input type="checkbox" name="rating" value="5" /> 5 ★
+                    <input type="checkbox" name="rating" value="4" /> 4 ★ & above
+                    <input type="checkbox" name="rating" value="3" /> 3 ★ & above
+                </div>
+                <div>
+                    <label>Area Range: </label>
+                    <input type="radio" name="area" value="1-2 Km" onChange={() => setAreaRange('1-2 Km')} /> Within 1-2 Km Area
+                    <input type="radio" name="area" value="2-5 Km" onChange={() => setAreaRange('2-5 Km')} /> Within 2-5 Km Area
+                    <input type="radio" name="area" value="5-10 Km" onChange={() => setAreaRange('5-10 Km')} /> Within 5-10 Km Area
+                </div>
+            </form>
+        </div>
+        </Dialog>
+    );
 };
 
 export default Search_form;
