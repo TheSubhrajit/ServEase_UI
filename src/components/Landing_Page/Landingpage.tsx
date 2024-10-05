6aff92b06dff2ffce9cee955dcfaf1a80723bc00
Subir-Droid <subirdeyabhi@gmail.com>
2024-10-05 11:12:36 +0530

    making lading page responsive

--- src/components/Landing_Page/Landingpage.tsx
+++ src/components/Landing_Page/Landingpage.tsx
@@ -1,33 +1,31 @@
+import React from "react";
 import { Tooltip } from "@mui/material";
 import "./Landingpage.css";
 
-
 interface ChildComponentProps {
-  sendDataToParent: (data: string) => void; // Adjust the type if needed
+  sendDataToParent: (data: string) => void;
 }
 
-export const Landingpage: React.FC<ChildComponentProps> = ({
-  sendDataToParent,
-}) => {
-  const handleClick = (e: any) => {
-    sendDataToParent(e);
+export const Landingpage: React.FC<ChildComponentProps> = ({ sendDataToParent }) => {
+  const handleClick = (data: string) => {
+    sendDataToParent(data);
   };
 
   return (
     <section className="landing-container">
       <Tooltip title="Cook" arrow>
-        <div className="selectors" onClick={(e) => handleClick("cook")}>
-          <img className="icon" src="../cooking.png"></img>
+        <div className="selectors" onClick={() => handleClick("cook")}>
+          <img className="icon" src="../cooking.png" alt="Cook" />
         </div>
       </Tooltip>
       <Tooltip title="Maid" arrow>
-        <div className="selectors" onClick={(e) => handleClick("maid")}>
-          <img className="icon" src="../cleaner.png"></img>
+        <div className="selectors" onClick={() => handleClick("maid")}>
+          <img className="icon" src="../cleaner.png" alt="Maid" />
         </div>
       </Tooltip>
       <Tooltip title="Nanny" arrow>
-        <div className="selectors" onClick={(e) => handleClick("nanny")}>
-          <img className="icon" src="../babysitter.png"></img>
+        <div className="selectors" onClick={() => handleClick("nanny")}>
+          <img className="icon" src="../babysitter.png" alt="Nanny" />
         </div>
       </Tooltip>
     </section>
