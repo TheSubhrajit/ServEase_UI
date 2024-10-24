6773180d88dc95dda67b7e3ac6d5af192443f359
My Name <thesubhrajitd2001@gmail.com>
2024-10-24 11:37:48 +0530

    Minor updates and user profile adding

--- src/components/Landing_Page/Landingpage.tsx
+++ src/components/Landing_Page/Landingpage.tsx
@@ -16,7 +16,7 @@ export const Landingpage: React.FC<ChildComponentProps> = ({ sendDataToParent })
        <div className="selector-container">
         <Tooltip title="Cook" arrow>
           <div className="selectors" onClick={() => handleClick("cook")}>
-            <img src="../cooking.png" alt="Cook" />
+            <img src="../cook_a.png" alt="Cook" />
           </div>
         </Tooltip>
         <p className="label-text">Cook</p> {/* Label below the circle */}
@@ -25,7 +25,7 @@ export const Landingpage: React.FC<ChildComponentProps> = ({ sendDataToParent })
       <div className="selector-container">
         <Tooltip title="Maid" arrow>
           <div className="selectors" onClick={() => handleClick("maid")}>
-            <img src="../cleaner.png" alt="Maid" />
+            <img src="../maid_a.png" alt="Maid" />
           </div>
         </Tooltip>
         <p className="label-text">Maid</p> {/* Label below the circle */}
@@ -34,7 +34,7 @@ export const Landingpage: React.FC<ChildComponentProps> = ({ sendDataToParent })
       <div className="selector-container">
         <Tooltip title="Nanny" arrow>
           <div className="selectors" onClick={() => handleClick("nanny")}>
-            <img src="../babysitter.png" alt="Nanny" />
+            <img src="../nanny_a.png" alt="Nanny" />
           </div>
         </Tooltip>
         <p className="label-text">Nanny</p> {/* Label below the circle */}
