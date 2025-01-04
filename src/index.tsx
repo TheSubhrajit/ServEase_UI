a1c093cd27fe0a3bec8a4518cbebfb1b6ab953a7
Subir-Droid <subirdeyabhi@gmail.com>
2025-01-04 19:46:44 +0530

    Admin to be different component

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,27 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from './store/userStore'
-import { Provider } from 'react-redux'
+import store from './store/userStore';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Importing routing components
+import Admin from "./components/Admin/Admin";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <Router>  {/* Wrapping the App with Router to enable routing */}
+        <Routes>
+          {/* Define the routes */}
+          <Route path="/" element={<App />} />  {/* Default route */}
+          <Route path="/admin" element={<Admin />} />  {/* Another view route */}
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
-  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
